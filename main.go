8b0e448c29a907f0eafba28e45a40e2dcc86d560
nowa <nowersvt@gmail.com>
2023-03-05 08:48:23 +0000

    Commit 3

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
         fmt.Fprintf(w, "<html><body style='background-color: %s; color: white'><h1>Hostname: %s</h1></body></html>", bgColor, hostname)
         w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
         w.Header().Set("Pragma", "no-cache")
-        w.Header().Set("Expires", "0")
+        w.Header().Set("Expires", "0")//
     })//
 
     if err := http.ListenAndServe(":8003", nil); err != nil {
